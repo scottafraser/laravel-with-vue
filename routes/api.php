860f3bc00187c7d732666316b18178c6af7f6abc
Scott Fraser <scottfraserdev@gmail.com>
2018-11-13 14:27:31 -0800

    through part 4

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+Route::resource('/task', 'TaskController');
+
+Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+
+Route::resource('/category', 'CategoryController');
+
+Route::post('login', 'UserController@login');
+
+Route::post('register', 'UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::resource('/task', 'TaskController');
+    Route::resource('/category', 'CategoryController');
+    Route::get('/category/{category}/tasks', 'CategoryController@tasks');
+});
